984571893c3c78f3898a3589e995e2527bf53b26
Eugene Ponizovsky <ponizovsky@gmail.com>
2018-08-03 12:11:05 +0300

    Formatted documentation

--- conf.go
+++ conf.go
@@ -67,19 +67,19 @@ func NewProcessor(config ProcessorConfig) *Processor {
 // conf tags defined in the struct type can indicate which fields the values are
 // mapped to (see the example). The decoder will make the following conversions:
 //
-// - bools to string (true = "1", false = "0")
-// - numbers to string (base 10)
-// - bools to int/uint (true = 1, false = 0)
-// - strings to int/uint (base implied by prefix)
-// - int to bool (true if value != 0)
-// - string to bool (accepts: 1, t, T, TRUE, true, True, 0, f, F,
-//   FALSE, false, False. Anything else is an error)
-// - empty array = empty map and vice versa
-// - negative numbers to overflowed uint values (base 10)
-// - slice of maps to a merged map
-// - single values are converted to slices if required. Each
-//   element is weakly decoded. For example: "4" can become []int{4}
-//   if the target type is an int slice.
+//  - bools to string (true = "1", false = "0")
+//  - numbers to string (base 10)
+//  - bools to int/uint (true = 1, false = 0)
+//  - strings to int/uint (base implied by prefix)
+//  - int to bool (true if value != 0)
+//  - string to bool (accepts: 1, t, T, TRUE, true, True, 0, f, F,
+//    FALSE, false, False. Anything else is an error)
+//  - empty array = empty map and vice versa
+//  - negative numbers to overflowed uint values (base 10)
+//  - slice of maps to a merged map
+//  - single values are converted to slices if required. Each
+//    element is weakly decoded. For example: "4" can become []int{4}
+//    if the target type is an int slice.
 func Decode(configRaw, config interface{}) error {
 	decoder, err := mapstruct.NewDecoder(
 		&mapstruct.DecoderConfig{
